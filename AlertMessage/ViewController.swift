7bf162609516f3366f4583aea0d9c0886da942b0
Faridho <faridho@zeepos.com>
2020-03-12 22:18:55 +0700

    Initial Commit

---
+++ AlertMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertMessage
+//
+//  Created by Faridho on 12/03/20.
+//  Copyright © 2020 Faridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
